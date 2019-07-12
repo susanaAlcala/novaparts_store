7c795ea3819f5eab1885aa7399730313eba731ef
Susana Alcala <susi.alcala@gmail.com>
2019-07-12 11:58:40 -0400

    Personalizacion de vistas de form de active_admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -12,6 +12,8 @@ ActiveAdmin.register User do
 #   permitted
 # end
 
+permit_params :email, :password
+
 index do
     column :id
     column :email
@@ -19,13 +21,28 @@ index do
     column :member_since do |user|
         time_ago_in_words(user.created_at)
     end
-  actions
+
+    actions
 end
  
 form do |f|
-    input :email
-    input :password
-    # actions
+    inputs 'Agregando un nuevo usuario' do
+        input :email
+        input :password
+    end
+    actions
 end
 
+controller do
+    def update
+      if (params[:user][:password].blank? && params[:user][:password_confirmation].blank?)
+      params[:user].delete("password")
+      params[:user].delete("password_confirmation")
+      end
+    super
+  end
+end
+
+filter :email, as: :select
+filter :created_at
 end
