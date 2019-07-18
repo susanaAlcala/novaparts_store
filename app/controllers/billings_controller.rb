87aa74fe84bd1c4a658a3ec8025ee57c93a866b2
Susana Alcala <susi.alcala@gmail.com>
2019-07-17 21:10:58 -0400

    Cambios en paypal.yml y billings_controller seran sobreescritos

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -1,65 +1,11 @@
 class BillingsController < ApplicationController
-  before_action :authenticate_user!
-
+    before_action :authenticate_user!
     def index
-        @billings = current_user.billings  
+        @billings = current_user.billings
     end
-
     def pre_pay
-        orders = current_user.orders.cart
-        total = orders.get_total
-        items = get_items_hash(orders)
-        
-        @payment = PayPal::SDK::REST::Payment.new({
-            intent: "sale",
-            payer: {
-                payment_method: "paypal" },
-                redirect_urls: {
-                    return_url: execute_billings_url,
-                    cancel_url: "http://localhost:3000/" },
-                    transactions: [{
-                        item_list: {
-                            items: items
-                        },
-                        amount: {
-                            total: total.to_s,
-                            currency: "USD" },
-                            description: "Carro de compra Novaparts Store" }]})
-                            
-                            if @payment.create
-                                redirect_url = @payment.links.find{|v| v.method == "REDIRECT"}.href
-                                redirect_to redirect_url
-                            else
-                                render json: @payment.error
-                            end
-                        end
-                        
-    def execute
-        paypal_payment = PayPal::SDK::REST::Payment.find(params[:paymentId])
-        
-        if paypal_payment.execute(payer_id: params[:PayerID])
-            
-            amount = paypal_payment.transactions.first.amount.total
-            
-            billing = Billing.create(
-                user: current_user,
-                code: paypal_payment.id,
-                payment_method: 'paypal',
-                amount: amount,
-                currency: 'USD'
-                )
-                
-            orders = current_user.orders.where(payed: false)
-            orders.update_all(payed: true, billing_id: billing.id)
-            
-            redirect_to root_path, notice: "La compra se realizó con éxito!"
-        else
-            render plain: "No se puedo generar el cobro en PayPal."
-        end
-    end
-    private 
-
-    def get_items_hash(orders)
+        orders = current_user.orders.where(payed: false)
+        total = orders.pluck("price * quantity").sum()
         items = orders.map do |order|
             item = {}
             item[:name] = order.product.name
@@ -70,7 +16,53 @@ class BillingsController < ApplicationController
             item
         end
         
+        if Rails.env.development?
+            redirect_urls_ok = {
+                return_url: "http://localhost:3000/billings/execute",
+                cancel_url: "http://localhost:3000/"  
+            }
+        else
+            redirect_urls_ok = {
+                return_url: "https://novaparts.herokuapp.com/billings/execute",
+                cancel_url: "https://novaparts.herokuapp.com/" 
+            }
+        end
+        
+        payment = PayPal::SDK::REST::Payment.new({
+            intent: "sale",
+            payer: {payment_method: "paypal" },
+            redirect_urls: redirect_urls_ok,
+            transactions: [{
+                item_list: {items: items },
+                amount: {total: total.to_s, currency: "USD" },
+                description: "Compra desde E-commerce Rails." 
+            }]
+        })
+
+        if payment.create
+            redirect_url = payment.links.find{|v| v.method == "REDIRECT"}.href
+            redirect_to redirect_url
+        else
+            render json: payment.error
+        end
     end
-    
+
+    def execute
+        paypal_payment = PayPal::SDK::REST::Payment.find(params[:paymentId])
+        if paypal_payment.execute(payer_id: params[:PayerID])
+            amount = paypal_payment.transactions.first.amount.total
+            billing = Billing.create(
+                user: current_user,
+                code: paypal_payment.id,
+                payment_method: 'paypal',
+                amount: amount,
+                currency: 'USD'
+            )
+            orders = current_user.orders.where(payed: false)
+            orders.update_all(payed: true, billing_id: billing.id)
+            redirect_to root_path, notice: "La compra se realizó con éxito!"
+        else
+            render plain: "No se puedo generar el cobro en PayPal."
+        end
+    end
 end
-                        
