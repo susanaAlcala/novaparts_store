854b5f631437ccbed3f96993c760d76ba07199d0
Susana Alcala <susi.alcala@gmail.com>
2019-07-18 15:15:00 -0400

    Migracion billing_order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_044336) do
+ActiveRecord::Schema.define(version: 2019_07_18_190941) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
